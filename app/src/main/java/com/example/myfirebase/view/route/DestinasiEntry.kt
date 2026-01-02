10374f764f6739f7f9e4de3f1b8f436950f3e97a
Ridhorzqullah <ridhorizqullah3@gmail.com>
2026-01-02 17:10:15 +0700

    docs: Menambahkan dokumentasi pada DestinasiEntry
    Menambahkan KDoc pada objek DestinasiEntry untuk menjelaskan rute navigasi halaman input.

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,11 @@
+package com.example.myfirebase.view.route
+
+import com.example.myfirebase.R
+
+/**
+ * Destinasi navigasi untuk halaman entry data siswa.
+ */
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
